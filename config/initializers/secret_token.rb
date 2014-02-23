871864a0e08018ced4c55e1d78bb8fcea47fb0f4
Satou Jun <j_sato@mse.biglobe.ne.jp>
2014-02-23 13:23:14 +0900

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThanksBank::Application.config.secret_token = '27abb20475c837c088c94ebefc256f9faa527d6d089d0e7d1cad7f0d4a94329a4761db4d77245cc41ecbcb6b3c4aeecd6c74948c3d0145427da41e4873455c49'
