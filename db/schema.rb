081dfd4b20f8b98a91b1f377d73caffa426d39eb
Satou Jun <j_sato@mse.biglobe.ne.jp>
2014-02-26 21:02:43 +0900

    give me と give you を追加しました。

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140223215356) do
+ActiveRecord::Schema.define(:version => 20140226115139) do
 
   create_table "users", :force => true do |t|
     t.string   "account_no"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(:version => 20140223215356) do
     t.text     "memo"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.text     "give_me"
+    t.text     "give_you"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,8 @@ user.account_no = '001'
 user.name = '佐藤順その一'
 user.area = '宇都宮市宝木町その一'
 user.memo = 'プログラマその一'
+user.give_me = '食べ物'
+user.give_you = '買い物の送迎'
 user.save!
 
 user = User.new
@@ -22,6 +24,8 @@ user.account_no = '002'
 user.name = '佐藤順その二'
 user.area = '宇都宮市宝木町その二'
 user.memo = 'プログラマその二'
+user.give_me = '食べ物'
+user.give_you = '買い物の送迎'
 user.save!
 
 user = User.new
@@ -29,6 +33,8 @@ user.account_no = '003'
 user.name = '佐藤順その三'
 user.area = '宇都宮市宝木町その三'
 user.memo = 'プログラマその三'
+user.give_me = '食べ物'
+user.give_you = '買い物の送迎'
 user.save!
 
 user = User.new
@@ -36,6 +42,8 @@ user.account_no = '004'
 user.name = '佐藤順その四'
 user.area = '宇都宮市宝木町その四'
 user.memo = 'プログラマその四'
+user.give_me = '食べ物'
+user.give_you = '買い物の送迎'
 user.save!
 
 user = User.new
@@ -43,6 +51,8 @@ user.account_no = '005'
 user.name = '佐藤順その五'
 user.area = '宇都宮市宝木町その五'
 user.memo = 'プログラマその五'
+user.give_me = '食べ物'
+user.give_you = '買い物の送迎'
 user.save!
 
 user = User.new
@@ -50,6 +60,8 @@ user.account_no = '006'
 user.name = '佐藤順その六'
 user.area = '宇都宮市宝木町その六'
 user.memo = 'プログラマその六'
+user.give_me = '食べ物'
+user.give_you = '買い物の送迎'
 user.save!
 
 user = User.new
@@ -57,6 +69,8 @@ user.account_no = '007'
 user.name = '佐藤順その七'
 user.area = '宇都宮市宝木町その七'
 user.memo = 'プログラマその七'
+user.give_me = '食べ物'
+user.give_you = '買い物の送迎'
 user.save!
 
 user = User.new
@@ -64,6 +78,8 @@ user.account_no = '008'
 user.name = '佐藤順その八'
 user.area = '宇都宮市宝木町その八'
 user.memo = 'プログラマその八'
+user.give_me = '食べ物'
+user.give_you = '買い物の送迎'
 user.save!
 
 user = User.new
@@ -71,6 +87,8 @@ user.account_no = '009'
 user.name = '佐藤順その九'
 user.area = '宇都宮市宝木町その九'
 user.memo = 'プログラマその九'
+user.give_me = '食べ物'
+user.give_you = '買い物の送迎'
 user.save!
 
 user = User.new
@@ -78,6 +96,8 @@ user.account_no = '010'
 user.name = '佐藤順その十'
 user.area = '宇都宮市宝木町その十'
 user.memo = 'プログラマその十'
+user.give_me = '食べ物'
+user.give_you = '買い物の送迎'
 user.save!
 
 user = User.new
@@ -85,6 +105,8 @@ user.account_no = '011'
 user.name = '佐藤順その十一'
 user.area = '宇都宮市宝木町その十一'
 user.memo = 'プログラマその十一'
+user.give_me = '食べ物'
+user.give_you = '買い物の送迎'
 user.save!
 
 user = User.new
@@ -92,6 +114,8 @@ user.account_no = '012'
 user.name = '佐藤順その十二'
 user.area = '宇都宮市宝木町その十二'
 user.memo = 'プログラマその十二'
+user.give_me = '食べ物'
+user.give_you = '買い物の送迎'
 user.save!
 
 user = User.new
@@ -99,5 +123,7 @@ user.account_no = '013'
 user.name = '佐藤順その十三'
 user.area = '宇都宮市宝木町その十三'
 user.memo = 'プログラマその十三'
+user.give_me = '食べ物'
+user.give_you = '買い物の送迎'
 user.save!
 
